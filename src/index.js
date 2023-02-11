4d01eaf9f75f75a792bd016df4376a246baf5736
Pedro Valério <pedroddjkK@gmail.com>
2023-02-11 07:44:27 -0300

    Navbar Component

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import NavBar from './components/navbar';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    
+    <NavBar />
   </React.StrictMode>
 );
 
