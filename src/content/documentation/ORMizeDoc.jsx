7b07b7b3caa7de971f6ece08c022be4ceb5d6640
Pedro Valério <pedroddjkK@gmail.com>
2023-02-14 06:16:09 -0300

    Content Section

--- src/content/documentation/ORMizeDoc.jsx
+++ src/content/documentation/ORMizeDoc.jsx
@@ -1,10 +1,20 @@
 const ORMizeDocumentation = () => {
   return (
     <>
-      <h1>ORMize Documentation</h1>
-      <p>ORMize is a TypeScript-developed Object-Relational Mapping (ORM), but can also be used in JavaScript. Its goal is to ease the interaction between applications and databases, allowing developers to work with data using objects instead of writing SQL code.Currently, ORMize only supports the MySQL database, but I plan to add support for more databases in the future.</p>
+      <div className="contentSection">
+        <h1>ORMize Documentation</h1>
+        <p>
+          ORMize is a TypeScript-developed Object-Relational Mapping (ORM), but
+          can also be used in JavaScript. Its goal is to ease the interaction
+          between applications and databases, allowing developers to work with
+          data using objects instead of writing SQL code.Currently, ORMize only
+          supports the MySQL database, but I plan to add support for more
+          databases in the future.
+        </p>
+      </div>
     </>
-  )
-}
+  );
+};
 
 export default ORMizeDocumentation;
+;
