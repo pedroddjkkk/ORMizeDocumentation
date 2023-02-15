bfdb6b242b140b721af92dc2e0b7cda902278356
Pedro Aparecido Valerio <pedroddjkk@gmail.com>
2023-02-15 10:11:06 -0300

    Applying the Href to NavBar

--- src/app.jsx
+++ src/app.jsx
@@ -6,10 +6,10 @@ const App = () => {
   return (
     <div>
       <NavBar>
-        <NavBar.Top>ORMize Documentation</NavBar.Top>
-        <NavBar.Item>Installing</NavBar.Item>
-        <NavBar.Item>Connecting</NavBar.Item>
-        <NavBar.Item>Model Classes</NavBar.Item>
+        <NavBar.Top href="#ormizeSection">ORMize Documentation</NavBar.Top>
+        <NavBar.Item href="#installingSection">Installing</NavBar.Item>
+        <NavBar.Item href="#connectionSection">Connecting</NavBar.Item>
+        <NavBar.Item href="#modelSection">Model Classes</NavBar.Item>
         <NavBar.Item>Finding Registers</NavBar.Item>
         <NavBar.Item>Saving fields</NavBar.Item>
       </NavBar>
